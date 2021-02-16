a70f9aa9c5dd9f96dfce267b8816811fd33b5f00
Mirko <mirko.rinelli@gmail.com>
2021-02-16 17:08:59 +0100

    Working on print db

--- db/user_databese.php
+++ db/user_databese.php
@@ -19,17 +19,28 @@ class Address_details {
 class User extends Address_details {
     public $name;
     public $lastname;
+    public $age;
     public $user_name;
     public $email_address;
     public $password;
     
-    public function __construct(string $name, string $lastname, string $user_name, string $email_address, string $password)
+    public function __construct(string $name, string $lastname, int $age, string $user_name, string $email_address, string $password)
     {
         $this->name = $name;
         $this->lastname = $lastname;
+        $this->age = $age;
         $this->user_name = $user_name;
         $this->email_address = $email_address;
         $this->password = $password;
     }
 }
+
+$user_list = 
+[
+    new User('Giovanni', 'Rossi', 31, 'giovannino.rossi89', 'giova@gmail.com', 'qwerty89'),
+    new User('Paolo', 'Bianchi', 29, 'bianchiPaul', 'paulB@live.com', 'admin23'),
+    new User('Elisa', 'Casablanca', 22, 'ElisaBlanca29', 'blanca@libero.it', 'eli81cuore'),  
+    new User('Federica', 'Sassuolo', 43, 'fedesass', 'sassuolo.F@gmail.com', '123FedeSass'),  
+    new User('Andrea', 'Biagio', 51, 'abiagio51', 'a_biagio@live.com', 'ytrewq00')
+]; 
 
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+  include __DIR__ . '\db\user_databese.php';
 
+  var_dump($user_list);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP 2</title>
+</head>
+<body>
+    
+</body>
+</html>
